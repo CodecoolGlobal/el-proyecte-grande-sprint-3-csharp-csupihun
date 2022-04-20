4fb768947a64062efc42ac0ae944229183b2c64b
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-04-20 12:20:46 +0200

    create dog

---
+++ ElProjecteGrande/Models/Dog.cs
@@ -0,0 +1,19 @@
+﻿namespace ElProjecteGrande.Models
+{
+    public class Dog
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public string Breed { get; }
+
+        public string Sex { get; }
+
+        public Dog(string name, int age, string breed, string sex)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+            Sex = sex;
+        }
+    }
+}
---
+++ ElProjecteGrande/Services/DogCreator.cs
@@ -0,0 +1,18 @@
+﻿using ElProjecteGrande.Models;
+
+namespace ElProjecteGrande.Services
+{
+    public class DogCreator
+    {
+        public static Dog CreateRandomDog()
+        {
+            string name = "placeholder";
+            string breed= "placeholder";
+            int age = new Random().Next(15);
+            List<string> sexes = new List<string>{ "male", "female" };
+            int sexIndex = new Random().Next(sexes.Count);
+            string sex = sexes[sexIndex];
+            return new Dog(name, age, breed, sex);
+        }
+    }
+}
