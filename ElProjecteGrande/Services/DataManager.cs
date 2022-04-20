9e411d31dfe66944f78afbaa3ec19eca05b78085
Csupihun <csupihun@gmail.com>
2022-04-20 12:33:23 +0200

    implement the ability to get data from outside api through GetDog

---
+++ ElProjecteGrande/Services/DataManager.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace ElProjecteGrande.Services
+{
+    public class DataManager
+    {
+        public static DataManager? Singleton { get; private set; } = null;
+
+        public static DataManager GetInstance
+        {
+            get
+            {
+                if (Singleton == null)
+                {
+                    Singleton = new DataManager();
+                }
+                return Singleton;
+            }
+        }
+
+        public Dictionary<string, string> ParseDogData(string dogString)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(dogString);
+        }
+    }
+}
