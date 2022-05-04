000bdbbea96f1e5dd44d1c2d3785e2089d6ad7c1
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-05-04 14:57:25 +0200

    add init swiping

--- tierreact/src/App.js
+++ tierreact/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card.js';
+import Swipe from './Swipe.js';
 import image from './image/TierPaw.png';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         <div className='Header-text'><button className='Header-button'><strong> Register </strong></button><button className='Header-button'><strong> Login </strong></button></div>
         
       </header>
-          <Card />
+          {/* <Card /> */}
+          <div className='Swipe-container'><Swipe /></div>
           <img className='logo' src={image} height={350}></img>
     </div>
   );
