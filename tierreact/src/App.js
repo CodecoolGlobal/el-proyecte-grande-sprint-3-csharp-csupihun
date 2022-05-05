475432882e919a5397616807821130c4ac3d6e7e
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-05-05 10:44:02 +0200

    add working swipe

--- tierreact/src/App.js
+++ tierreact/src/App.js
@@ -1,20 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Registration from './Registration.js';
-import Swipe from './Swipe.js';
+import Card from './Card.js';
 import image from './image/TierPaw.png';
 import { useState } from 'react';
+import TinderCard from 'react-tinder-card';
 
 function App() {
   const [formstate, setFormState] = useState(false)
   const ShowRegistration = () => {setFormState(!formstate)}
+  const onSwipe = (direction) => {
+    console.log('You swiped: ' + direction)
+  }
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + ' left the screen')
+  } 
   return (
     <div className="App">
       <header className="App-header">
         <div className='Header-text'><button onClick={ShowRegistration} className='Header-button'><strong> Register </strong></button><button className='Header-button'><strong> Login </strong></button></div>
         
       </header>
-          <div className='Swipe-container'><Swipe /></div>
+      <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('dogCard')} ><div className='Swipe-container'><Card /></div></TinderCard>
           <img className='logo' src={image} height={350}></img>
           <Registration  state={formstate} stateChange={ShowRegistration}/>
     </div>
