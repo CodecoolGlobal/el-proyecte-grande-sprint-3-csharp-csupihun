3cad166a9822ec4ded9bb88f124671cf80c06d90
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-05-05 10:42:01 +0200

    change heart color

--- tierreact/src/Card.js
+++ tierreact/src/Card.js
@@ -27,7 +27,7 @@ const Card = () => {
       <div className='Dog-image'><img className='Api-image' src={currentDog.Picture} /><br /></div>
       <div className='Dog-container'><div className='Dog-name'><strong>{currentDog.Name}, {currentDog.Age} </strong></div><div className='Dog-sex'><strong>{currentDog.Sex}</strong></div><br /></div>
       <div className='Dog-breed'><strong>{currentDog.Breed}</strong><br /></div>
-              <div className='Buttons'><button onClick={GetDogData} className='Swipe-button' >❌</button> <button className='Swipe-button' onClick={LikeDog} >❤️</button></div></div>
+              <div className='Buttons'><button onClick={GetDogData} className='Swipe-button' >❌</button> <button className='Swipe-button' onClick={LikeDog} >💚</button></div></div>
   )
 }
 
