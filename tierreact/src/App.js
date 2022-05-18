499518b03bf3eb020263942a159e7fabe3248fd7
Natalia Alemany <nataliaalemanybota@gmail.com>
2022-05-18 14:33:40 +0200

    add onClick to login button

--- tierreact/src/App.js
+++ tierreact/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Registration from './Registration.js';
+import Login from './Login.js';
 import Card from './Card.js';
 import logo from './image/TierPaw.png';
 import { useState } from 'react';
@@ -7,8 +8,10 @@ import TinderCard from 'react-tinder-card';
 
 function App() {
   const [swipeState, setSwipeState] = useState(false)
-  const [formstate, setFormState] = useState(false)
-  const ShowRegistration = () => {setFormState(!formstate)}
+  const [registerState, setRegisterState] = useState(false)
+  const [loginState, setLoginState] = useState(false)
+  const ShowRegistration = () => {setRegisterState(!registerState)}
+  const ShowLogin = () => {setLoginState(!loginState)}
   const onSwipe = (direction) => {
     console.log('You swiped: ' + direction)
     if (direction == 'left' || direction == 'down') {
@@ -38,12 +41,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='Header-text'><button onClick={ShowRegistration} className='Header-button'>
-          <strong> Register </strong></button><button className='Header-button'><strong> Login </strong>
+          <strong> Register </strong></button><button onClick={ShowLogin} className='Header-button'><strong> Login </strong>
         </button></div>   
       </header>
           <CardManager swipeState={swipeState} setSwipeState={setSwipeState}/>
           <img className='logo' src={logo} height={350}></img>
-          <Registration  state={formstate} stateChange={ShowRegistration}/>
+          <Registration  state={registerState} stateChange={ShowRegistration}/>
+          <Login  state={loginState} stateChange={ShowLogin} registerButton={ShowRegistration}/>
     </div>
   );
 }
